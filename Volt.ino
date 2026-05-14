66543189a3df8935feea444e97c5ef86015f5f54
Роман Тихонов <99688019+LazyPythonCoder@users.noreply.github.com>
2026-05-14 10:54:09 +0300

    Update Volt.ino
    Add red led

--- Volt.ino
+++ Volt.ino
@@ -6,8 +6,9 @@ const int analogPinLD = A2; // Пин для проверки напряжени
 const int greenLed = 2;     // Зеленый светодиод
 const int redLed = 3;       // Красный светодиод
 const int relayPin = 4;
-const int sensorPhoto = 5;
+const int sensorPhotoBlue = 5;
 const int relayPin2 = 6;
+const int sensorPhotoRed = 7;
 
 
 // 4 минуты в миллисекундах (4 * 60 * 1000)
@@ -16,7 +17,8 @@ const unsigned long waitTime4 = 242000;
 
 bool checked2 = false;
 bool checked4 = false;
-bool checkedLed = false;
+bool bluecheckedLed = false;
+bool redcheckedLed = false;
 bool checkedV = false;
 
 int rawValueLD = 0;
@@ -35,7 +37,8 @@ void setup() {
   pinMode(redLed, OUTPUT);
   pinMode(relayPin, OUTPUT);
   pinMode(relayPin2, OUTPUT);
-  pinMode(sensorPhoto, INPUT); 
+  pinMode(sensorPhotoBlue, INPUT); 
+  pinMode(sensorPhotoRed, INPUT); 
 
   lcd.init();           // Инициализация дисплея
   lcd.backlight();      // Включаем подсветку
@@ -62,22 +65,22 @@ void loop() {
   if (!checked2 && millis() >= waitTime2) {
     lcd.clear();
     lcd.setCursor(0, 0);
-    int led = digitalRead(sensorPhoto);
+    int led = digitalRead(sensorPhotoBlue);
     Serial.println(led);
     
     if (led == 0) {
-      lcd.print("Led checked");
-      Serial.println("Led checked");
-      checkedLed = true;
+      lcd.print("Blue led checked");
+      Serial.println("Blue led checked");
+      bluecheckedLed = true;
     } else {
-      lcd.print("LED failed");
-      Serial.println("Led failed");
+      lcd.print("Blue led failed");
+      Serial.println("Blue led failed");
     }
     checked2 = true;
   }
 
   if (!checked4 && millis() >= waitTime4) {
-    lcd.setCursor(0, 1);
+    // lcd.setCursor(0, 1);
     digitalWrite(relayPin, HIGH);
     digitalWrite(relayPin2, HIGH);
     delay(700);
@@ -85,11 +88,25 @@ void loop() {
     rawValueLD2 = analogRead(analogPinLD);
     rawValueLD3 = analogRead(analogPinLD);
     rawValueLD = average(rawValueLD1, rawValueLD2, rawValueLD3);
+    int ledred = digitalRead(sensorPhotoRed);
+    Serial.print("Led red=");
+    Serial.println(ledred);
+    lcd.setCursor(0, 0);
+    if (ledred == 0) {
+      Serial.println("Led red passed");
+      redcheckedLed = true;
+      lcd.print("Red led checked");
+    } else {
+      Serial.println("Led red failed");
+      lcd.print("Red led failed");
+    }
+
     Serial.println(rawValueLD1 * (5.0 / 1023.0));
     Serial.println(rawValueLD2 * (5.0 / 1023.0));
     Serial.println(rawValueLD3 * (5.0 / 1023.0));
     Serial.println(rawValueLD);
     voltageLD = rawValueLD * (5.0 / 1023.0);
+    lcd.setCursor(0, 1);
     lcd.print("voltageLD=");
     lcd.print(voltageLD);
     lcd.print("V"); 
@@ -110,9 +127,10 @@ void loop() {
   
   Serial.println("Finish");
   Serial.print("Checked");
-  Serial.print(checkedLed);
+  Serial.print(bluecheckedLed);
+  Serial.print(redcheckedLed);
   Serial.println(checkedV);
-  if (checkedLed && checkedV) {
+  if (bluecheckedLed && checkedV && redcheckedLed) {
     digitalWrite(greenLed, HIGH);
     Serial.println("PASSED");
   } else {
@@ -137,3 +155,4 @@ float average(int a, int b, int c) {
   }
   return closest;
 }
+
